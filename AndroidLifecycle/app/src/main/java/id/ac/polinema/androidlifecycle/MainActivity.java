c1cce619f1f93d5d07d3894789dfc715b8491ae6
imamrozi <imam.rozi@polinema.ac.id>
2019-09-02 05:17:00 +0700

    app OnStart() and app onStop() modified

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package id.ac.polinema.androidlifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +14,18 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	// TODO: tambahkan callback onStart() di sini
-
+	@Override
+	protected void onStart(){
+		super.onStart();
+		Toast.makeText(this, "App onStart", Toast.LENGTH_SHORT).show();
+	}
 
 	// TODO: tambahkan callback onStop() di sini
+	@Override
+	protected void onStop(){
+		super.onStop();
+		Toast.makeText(this, "App onStop", Toast.LENGTH_SHORT).show();
+	}
 
 	// TODO: lengkapi callback untuk onRestart() onResume() onPause() onDestroy()
 }
